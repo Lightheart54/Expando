872180534eb9bda61517d638b226ce93b7eb21a6
Cullen Sarles <cullen.sarles@ryancompanies.com>
2024-08-21 14:05:16 -0500

    Modernize Project Files

--- Expando/Properties/AssemblyInfo.cs
+++ Expando/Properties/AssemblyInfo.cs
@@ -2,17 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Expando")]
-[assembly: AssemblyDescription("Expando: Extensible, dynamic types for .NET")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Rick Strahl, West Wind Technologies")]
-[assembly: AssemblyProduct("Expando")]
-[assembly: AssemblyCopyright("Copyright © West Wind Technologies 2012-2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -22,15 +11,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a2bd8f9e-888a-40dc-ba94-28558d26bf2e")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
